b03a971c49bffa7b89d67d3d66f6aecf8954cc9c
phuongtq <tranphuong.080@gmail.com>
2023-12-29 11:57:53 +0700

    fix(posthandler): [TTC-97] only apply refund_gas_remaining for normal cosmos txs and ExtensionOptionDynamicFeeTx

--- app/posthandler/posthandler.go
+++ app/posthandler/posthandler.go
@@ -1,7 +1,15 @@
 package posthandler
 
 import (
+	"fmt"
+	"runtime/debug"
+
+	tmlog "github.com/cometbft/cometbft/libs/log"
+
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	errortypes "github.com/cosmos/cosmos-sdk/types/errors"
+	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
 	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	bankkeeper "github.com/cosmos/cosmos-sdk/x/bank/keeper"
 	feegrantkeeper "github.com/cosmos/cosmos-sdk/x/feegrant/keeper"
@@ -12,11 +20,65 @@ func NewPostHandler(
 	bankKeeper bankkeeper.Keeper,
 	feegrantKeeper feegrantkeeper.Keeper,
 ) sdk.PostHandler {
-	postDecorators := []sdk.PostDecorator{
-		// The refund gas remaining decorator must be the last decorator in this
-		// list.
-		NewRefundGasRemainingDecorator(accountKeeper, bankKeeper, feegrantKeeper),
-	}
+	return func(
+		ctx sdk.Context, tx sdk.Tx, sim bool, success bool,
+	) (newCtx sdk.Context, err error) {
+		var postHandler sdk.PostHandler
 
-	return sdk.ChainPostDecorators(postDecorators...)
+		defer Recover(ctx.Logger(), &err)
+
+		postDecorators := []sdk.PostDecorator{
+			// The refund gas remaining decorator must be the last decorator in this
+			// list.
+			NewRefundGasRemainingDecorator(accountKeeper, bankKeeper, feegrantKeeper),
+		}
+
+		txWithExtensions, ok := tx.(authante.HasExtensionOptionsTx)
+		if ok {
+			opts := txWithExtensions.GetExtensionOptions()
+			if len(opts) > 0 {
+				switch typeURL := opts[0].GetTypeUrl(); typeURL {
+				case "/ethermint.evm.v1.ExtensionOptionsEthereumTx":
+					return ctx, nil
+				case "/ethermint.types.v1.ExtensionOptionDynamicFeeTx":
+					// cosmos-sdk tx with dynamic fee extension
+					postHandler = sdk.ChainPostDecorators(postDecorators...)
+					return postHandler(ctx, tx, sim, success)
+				default:
+					return ctx, errorsmod.Wrapf(
+						errortypes.ErrUnknownExtensionOptions,
+						"rejecting tx with unsupported extension option: %s", typeURL,
+					)
+				}
+			}
+		}
+
+		// handle as totally normal Cosmos SDK tx
+		switch tx.(type) {
+		case sdk.Tx:
+			postHandler = sdk.ChainPostDecorators(postDecorators...)
+			return postHandler(ctx, tx, sim, success)
+		default:
+			return ctx, errorsmod.Wrapf(errortypes.ErrUnknownRequest, "invalid transaction type: %T", tx)
+		}
+	}
+}
+
+func Recover(logger tmlog.Logger, err *error) {
+	if r := recover(); r != nil {
+		*err = errorsmod.Wrapf(errortypes.ErrPanic, "%v", r)
+
+		if e, ok := r.(error); ok {
+			logger.Error(
+				"post handler panicked",
+				"error", e,
+				"stack trace", string(debug.Stack()),
+			)
+		} else {
+			logger.Error(
+				"post handler panicked",
+				"recover", fmt.Sprintf("%v", r),
+			)
+		}
+	}
 }
