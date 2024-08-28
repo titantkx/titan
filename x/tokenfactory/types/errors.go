a104ceca399d063cbf30c692fc601e64750da426
Doan Phi Hung <hugjobk@gmail.com>
2024-08-28 15:04:36 +0700

    fix(tokenfactory): remove unused error ErrBeforeSendHookOutOfGas

--- x/tokenfactory/types/errors.go
+++ x/tokenfactory/types/errors.go
@@ -19,5 +19,4 @@ var (
 	ErrSubdenomTooLong          = errorsmod.Register(ModuleName, 8, fmt.Sprintf("subdenom too long, max length is %d bytes", MaxSubdenomLength))
 	ErrCreatorTooLong           = errorsmod.Register(ModuleName, 9, fmt.Sprintf("creator too long, max length is %d bytes", MaxCreatorLength))
 	ErrDenomDoesNotExist        = errorsmod.Register(ModuleName, 10, "denom does not exist")
-	ErrBeforeSendHookOutOfGas   = errorsmod.Register(ModuleName, 12, "gas meter hit maximum limit")
 )
