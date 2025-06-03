7a4d6c1bd0d4292d98abe65ead7698b4dc8fd5bd
phuongtq <tranphuong.080@gmail.com>
2025-06-03 15:34:51 +0700

    feat(precompiles/pointer): remove unnecessary check

--- precompiles/pointer/pointer.go
+++ precompiles/pointer/pointer.go
@@ -99,11 +99,6 @@ func (p PrecompileExecutor) AddNative(
 
 	token := args[0].(string)
 
-	// not allow to create pointer for base token (atkx)
-	if token == utils.BaseDenom {
-		return nil, fmt.Errorf("cannot create pointer for base token %s", utils.BaseDenom)
-	}
-
 	metadata, metadataExists := p.bankKeeper.GetDenomMetaData(ctx, token)
 	if !metadataExists {
 		return nil, fmt.Errorf("denom %s does not have metadata stored", token)
