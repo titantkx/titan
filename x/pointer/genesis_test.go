39e61bb168e8d9e62f3d8cf8aa3a69f18ef507ae
phuongtq <tranphuong.080@gmail.com>
2025-07-10 15:32:32 +0700

    test: correct genesis_test

--- x/pointer/genesis_test.go
+++ x/pointer/genesis_test.go
@@ -4,6 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	etherminttests "github.com/titantkx/ethermint/tests"
+
 	keepertest "github.com/titantkx/titan/testutil/keeper"
 	"github.com/titantkx/titan/testutil/nullify"
 	"github.com/titantkx/titan/x/pointer"
@@ -17,9 +20,15 @@ func TestGenesis(t *testing.T) {
 		Erc20NativeList: []types.Erc20Native{
 			{
 				TokenDenom: "0",
+				Erc20Addr:  etherminttests.GenerateAddress().String(),
 			},
 			{
 				TokenDenom: "1",
+				Erc20Addr:  etherminttests.GenerateAddress().String(),
+			},
+			{
+				TokenDenom: "2",
+				// empty Erc20Addr to test ignore this record
 			},
 		},
 		// this line is used by starport scaffolding # genesis/test/state
@@ -30,9 +39,11 @@ func TestGenesis(t *testing.T) {
 	got := pointer.ExportGenesis(ctx, *k)
 	require.NotNil(t, got)
 
+	// remove record that do not have Erc20Addr
+	shouldImportedErc20NativeList := append([]types.Erc20Native{}, genesisState.Erc20NativeList[:2]...)
+	require.ElementsMatch(t, shouldImportedErc20NativeList, got.Erc20NativeList)
+	// this line is used by starport scaffolding # genesis/test/assert
+
 	nullify.Fill(&genesisState)
 	nullify.Fill(got)
-
-	require.ElementsMatch(t, genesisState.Erc20NativeList, got.Erc20NativeList)
-	// this line is used by starport scaffolding # genesis/test/assert
 }
